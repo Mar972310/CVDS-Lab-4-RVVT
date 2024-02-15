d45e72da91a8fb5de8b07e6db29c96756db89e3b
unknown <jose.vasquez-v@labinfo.is.escuelaing.edu.co>
2024-02-15 17:05:17 -0500

    especificación métodos

---
+++ src/main/java/hangman/model/Implementation/BonusScore.java
@@ -0,0 +1,19 @@
+package hangman.model.Implementation;
+
+public class BonusScore {
+    /**
+     * Este metodo calcula si el puntaje de acuerdo con las letras correctas e incorrectas.
+     *
+     * @pre Inicia con 0 puntos.
+     * @post El puntaje minimo es 0
+     * @param correctCount Cantidad de letras acertadas .
+     * @param incorrectCount Cantidad de letras incorrectas.
+     * @return retorna un entero con el puntaje obtenido, la pelanización de 5 puntos por letra incorrecta y
+     * por una bonificación de 10 puntos por letra correcta.
+     */
+    public int calculateScore(int correctCount, int incorrectCount){
+
+        return 0;
+    }
+
+}
---
+++ src/main/java/hangman/model/Implementation/PowerBonusScore.java
@@ -0,0 +1,19 @@
+package hangman.model.Implementation;
+
+public class PowerBonusScore {
+
+    /**
+     * Este metodo calcula si el puntaje de acuerdo con las letras correctas e incorrectas.
+     *
+     * @pre Inicia con 0 puntos.
+     * @post El puntaje maximo es 500 puntos, si sobrepasa sera 500 punto igualmente.
+     * @param correctCount Cantidad de letras acertadas .
+     * @param incorrectCount Cantidad de letras incorrectas.
+     * @return retorna un entero con el puntaje obtenido y La $i-ésima$ letra correcta se bonifica con $5^i$ y
+     * y penalización de 8 puntos por cada letra incorrecta.
+     */
+    public int calculateScore(int correctCount, int incorrectCount){
+
+        return 0;
+    }
+}
